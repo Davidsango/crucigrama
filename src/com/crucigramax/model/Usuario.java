4d5e1af1bf3bb566fbfb8e1d206bfb87e6f09a97
aecardenasfl <aecardenasfl@poligran.edu.co>
2024-04-16 12:00:34 -0500

    Se agrega documentación a las clases, se generan constructores, setters y getters. Se agregó la lógica para llenar el crucigrama a partir de una cadena de caracteres (a obtener de la base de datos), así como la lógica para recorrerlo y generar la lista de los enunciados para las palabras horizontales y verticales.

--- src/com/crucigramax/model/Usuario.java
+++ src/com/crucigramax/model/Usuario.java
@@ -1,25 +1,62 @@
 package com.crucigramax.model;
+
 import java.util.*;
 
 /**
- * 
+ * La clase Usuario representa un usuario que participa en el juego de
+ * crucigrama, con un apodo (nickname) y una lista de puntajes obtenidos.
  */
 public class Usuario {
 
+    private String nickname; // El apodo del usuario
+    private List<Score> puntaje; // La lista de puntajes obtenidos por el usuario
+
     /**
-     * Default constructor
+     * Constructor de la clase Usuario.
+     *
+     * @param nickname El apodo del usuario.
+     * @param puntaje La lista de puntajes obtenidos por el usuario.
      */
-    public Usuario() {
+    public Usuario(String nickname, List<Score> puntaje) {
+        this.nickname = nickname;
+        this.puntaje = puntaje;
     }
 
     /**
-     * 
+     * Obtiene el apodo del usuario.
+     *
+     * @return El apodo del usuario.
      */
-    private String nickname;
+    public String getNickname() {
+        return nickname;
+    }
 
     /**
-     * 
+     * Establece el apodo del usuario.
+     *
+     * @param nickname El apodo del usuario a establecer.
      */
-    private List<Score> puntaje;
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    /**
+     * Obtiene la lista de puntajes obtenidos por el usuario.
+     *
+     * @return La lista de puntajes obtenidos por el usuario.
+     */
+    public List<Score> getPuntaje() {
+        return puntaje;
+    }
+
+    /**
+     * Establece la lista de puntajes obtenidos por el usuario.
+     *
+     * @param puntaje La lista de puntajes obtenidos por el usuario a
+     * establecer.
+     */
+    public void setPuntaje(List<Score> puntaje) {
+        this.puntaje = puntaje;
+    }
 
 }
